500aa87047825e2d8f82ae7b3a412444b05ac77b
LuizGuilherme13 <luizguilherme.bs13@hotmail.com>
2021-05-29 14:50:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mini_Curso_html_Banco_Inter
+ 
---
+++ js/main.js
@@ -0,0 +1,30 @@
+//alert("Aceitar Cookies da página?");
+//var receber_novidades = document.querySelector("#receber_novidades");
+
+var receber_novidades = document.getElementById("receber_novidades");
+
+receber_novidades.addEventListener("click", function(){
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var autorizo_conta = document.getElementById("autorizo_conta").checked;
+
+    if(!nome || !nome.toString().trim()){
+        alert("Informe o nome, por favor.");
+        return;
+    }
+
+    if(nome.lenght < 5){
+        alert("Informe um nome válido, por favor.")
+    }
+
+    if(!email || !email.toString().trim()){
+        alert("Informe o e-mail, por favor.");
+        return;
+    }
+
+    if(!autorizo_conta){
+        alert("Autorize por favor.");
+        return;
+    }
+
+});
